75043aa6ad23373545e02630ba7b0960c3aa2ed7
Rafael Rasquinho <rafael.srasquinho@gmail.com>
2023-10-13 01:27:44 -0300

    Construcao da Model

---
+++ src/main/java/model/Endereco.java
@@ -0,0 +1,71 @@
+package model;
+
+public class Endereco {
+
+    private String cep;
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String estado;
+    private String complemento;
+
+    // Construtor
+    public Endereco(String cep, String rua, String numero, String bairro, String estado, String complemento) {
+        this.cep = cep;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.estado = estado;
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
+
+
