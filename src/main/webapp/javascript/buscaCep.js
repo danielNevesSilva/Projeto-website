0caefe22af813fe18131a8b17bd4fdca5cd7c1ad
Rafael Rasquinho <rafael.srasquinho@gmail.com>
2023-10-16 23:11:16 -0300

    Ao inseri o CEP as demais informações serão completadas do Cadastro do Endereco e do Faturamento

--- src/main/webapp/javascript/buscaCep.js
+++ src/main/webapp/javascript/buscaCep.js
@@ -1,23 +1,22 @@
-async function getAddressByCEP() {
-    const cep = document.getElementById('cep').value.replace(/\D/g, '');
-
-    if (cep.length === 8) {
-        try {
-            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-            const data = await response.json();
-
-            if (!data.erro) {
-                document.getElementById('rua').value = data.logradouro;
-                document.getElementById('bairro').value = data.bairro;
-                document.getElementById('cidade').value = data.localidade;
-                document.getElementById('uf').value = data.uf;
-
-            } else {
-                alert('CEP não encontrado. Por favor, verifique o CEP inserido.');
-            }
-        } catch (error) {
-            console.error('Erro ao buscar endereço:', error);
+document.addEventListener('DOMContentLoaded', function() {
+    var cepInput = document.getElementById('cep');
+    cepInput.addEventListener('blur', function() {
+        var cepValue = cepInput.value.replace(/\D/g, '');
+        if (cepValue.length === 8) {
+            fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
+                .then(response => response.json())
+                .then(data => {
+                    if (!data.erro) {
+                        document.getElementById('rua').value = data.logradouro;
+                        document.getElementById('numero').focus();
+                        document.getElementById('bairro').value = data.bairro;
+                        document.getElementById('cidade').value = data.localidade;
+                        document.getElementById('uf').value = data.uf;
+                    } else {
+                        alert('CEP não encontrado. Por favor, verifique o CEP inserido.');
+                    }
+                })
+                .catch(error => console.error('Erro ao buscar endereço:', error));
         }
-    }
-}
-
+    });
+});
